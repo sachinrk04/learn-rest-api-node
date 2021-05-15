a465384f752f924dcffe0b2b734c54211d90dd5e
Sachin Singh <sachin.innoventes@phonepe.com>
2021-05-16 00:40:29 +0530

    async and await done

--- controllers/feed.js
+++ controllers/feed.js
@@ -6,33 +6,25 @@ const { validationResult } = require('express-validator');
 const Post = require('../modals/post');
 const User = require('../modals/user');
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
     const currentPage = req.query.page || 1;
     const perPage = 2;
-    let totalItems;
-    Post.find()
-        .countDocuments()
-        .then(count => {
-            totalItems = count;
-            return Post.find()
+    try {
+        const totalItems = await Post.find().countDocuments();
+        const posts = await Post.find()
             .skip((currentPage - 1) * perPage)
             .limit(perPage)
-        })
-        .then(posts => {
-            res
-                .status(200)
-                .json({
-                    message: 'Fetched posts successfully.', 
-                    posts: posts,
-                    totalItems: totalItems
-                });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
+        res.status(200).json({
+            message: 'Fetched posts successfully.', 
+            posts: posts,
+            totalItems: totalItems
         });
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = 500;
+        }
+        next(error);
+    }
 };
 
 exports.createPosts = (req, res, next) => {
